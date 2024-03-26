77fd919eaeae961f370ad4f43e39297ae7449f5b
MaguinhoMD <125109804+B4K123@users.noreply.github.com>
2024-03-26 15:19:17 -0300

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,4 +1,21 @@
 import { execSync } from 'child_process'
+let handler = async (m, { conn, text }) => {
+try {  
+if (global.conn.user.jid == conn.user.jid) {
+let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
+conn.reply(m.chat, stdout.toString(), m)}
+//} catch {
+var update = execSync('git remote set-url origin https://github.com/B4K123/SakuraBotLite-MD.git && git pull')
+await m.reply(update.toString())
+} catch {
+await m.reply(`${fg}`) 
+}}
+handler.help = ['update']
+handler.tags = ['owner']
+handler.command = /^update|actualizar$/i
+handler.rowner = true
+export default handler
+/*import { execSync } from 'child_process'
 
 var handler = async (m, { conn, text }) => {
 await conn.sendMessage(m.chat, { react: { text: '🍭', key: m.key } })
