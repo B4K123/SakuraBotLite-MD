bbc65f18b34216ff994fa63f4c69eda2714d10b2
MaguinhoMD <125109804+B4K123@users.noreply.github.com>
2024-03-26 16:00:08 -0300

    Update autoresponder.js

--- plugins/autoresponder.js
+++ plugins/autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen8, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: '𝐐𝐔𝐄 𝐘𝐎 𝐐𝐔𝐄?', body: '𝑨𝑯𝑶𝑹𝑨 𝑸𝑼𝑬 𝑸𝑼𝑰𝑬𝑹𝑬𝑺?', sourceUrl: `https://github.com/B4K123/SakuraBotLite-MD`, thumbnail: imagen7}}})}
+contextInfo: { externalAdReply: { title: '𝙁𝘼𝙇𝘼 𝙆𝘼𝙍𝘼𝙄 𝙀𝙐 𝙏𝙊̂ 𝘼𝙌𝙐𝙄?', body: '𝙊 𝙌𝙐𝙀 𝙑𝙊𝘾𝙀̂ 𝙌𝙐𝙀𝙍 𝘼𝙂𝙊𝙍𝘼??', sourceUrl: `https://github.com/B4K123/SakuraBotLite-MD`, thumbnail: imagen7}}})}
 
 return !0 }
 export default handler
